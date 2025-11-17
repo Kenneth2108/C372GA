5d1c490bb59ca8ed66c2ff1e4d376b5d27f9f12a
24000923 <24000923@myrp.edu.sg>
2025-11-17 14:57:59 +0800

    2nd DB Comments

--- db.js
+++ db.js
@@ -9,7 +9,7 @@ const db = mysql.createConnection({
     database: process.env.DB_NAME
 });
 
-//Connecting to SQL database
+//Connecting to MySQL database
 db.connect((err) => {
     if (err) {
         console.error('Error connecting to MySQL:', err);
